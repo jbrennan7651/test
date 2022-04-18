71db85713997b3d4db2a07aff6b83b38442356e5
jbrennan7651 <103645942+jbrennan7651@users.noreply.github.com>
2022-04-18 13:52:47 -0400

    Test 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World. This is update 1");
+        System.out.println("Hello World. This is update 1, test 2");
     }
 }
